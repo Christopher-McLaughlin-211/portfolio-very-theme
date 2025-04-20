f768335010c1e91abf1028aed080667668af9b74
Christopher-McLaughlin-211 <clm6781@psu.edu>
2025-04-20 17:57:17 -0400

    commit

--- portfolio-very-theme.js
+++ portfolio-very-theme.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import '@haxtheweb/scroll-button/scroll-button.js';
 
 /**
  * `portfolio-very-theme`
@@ -59,6 +60,7 @@ export class PortfolioVeryTheme extends DDDSuper(I18NMixin(LitElement)) {
   <slot></slot>
 </div>`;
   }
+
 
   /**
    * haxProperties integration via file reference
